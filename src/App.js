bd449626d3a50e53c6f17304d3b5b18b1f904041
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-02-14 19:45:05 +0100

    Added the Currency and Value to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Card, Dimmer, Loader, Select } from "semantic-ui-react";
 import "./App.css";
+
+const currencys = [
+  { value: "USD", text: "USD" },
+  { value: "EUR", text: "EUR" },
+  { value: "GBP", text: "GBP" },
+];
 
 function App() {
   const [display, setDisplay] = useState(true);
@@ -18,16 +25,52 @@ function App() {
     }
     fetchData();
   }, []);
+
+  const handleCurrency = (e, data) => {
+    setCurrency(data.value);
+  };
+
   return (
     <div className="App">
       <div className="nav" style={{ padding: 15, backgroundColor: "#FFBD35" }}>
         Bitcoin Price
       </div>
       {display ? (
-        <div></div>
+        <div>
+          <Dimmer active inverted>
+            <Loader>Loading...</Loader>
+          </Dimmer>
+        </div>
       ) : (
         <>
-          <div className="price-box"></div>
+          <div
+            className="price-box"
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              width: 600,
+              height: 300,
+            }}
+          >
+            <div className="form">
+              <Select
+                placeholder="Select Currency"
+                onChange={handleCurrency}
+                options={currencys}
+              ></Select>
+            </div>
+            <div className="value">
+              <Card>
+                <Card.Content>
+                  <Card.Header>{currency} Currency</Card.Header>
+                  <Card.Description>
+                    {coinPrice[currency].rate}
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
+          </div>
         </>
       )}
     </div>
